3c69ff3a89a78d4ae43cbbdb4ed6380cebd0fded
Abdullahkhalil81988 <akhalil.bsai24seecs@seecs.edu.pk>
2026-03-31 20:12:50 +0500

    os

--- node/index.js
+++ node/index.js
@@ -1,3 +1,8 @@
  const lovish = require('./utils');
 
  console.log('Hello,', lovish);
+ const os = require('os');
+ console.log('Operating System:', os.platform());
+ console.log('CPU Architecture:', os.arch());
+ console.log('Total Memory:', os.totalmem());
+ console.log('Free Memory:', os.freemem());

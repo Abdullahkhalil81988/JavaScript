41738fe546be7c201ece3fffc8c84e4dab844357
Abdullahkhalil81988 <akhalil.bsai24seecs@seecs.edu.pk>
2026-04-01 00:20:42 +0500

    server

--- node/httpserver.js
+++ node/httpserver.js
@@ -1,9 +1,9 @@
 const http = require('http');
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>helooooo abdullah</h1>');
+    res.end('<h1>helooooo abdullah <br> kia hal ha </h1>');
 
 })
 server.listen(port, () => {
